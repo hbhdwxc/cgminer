af17c4b54b2bc0f51449c76b99d653e805232156
Wangxc <wangxiaochuan@canaan-creative.com>
2018-12-25 20:35:30 +0800

    Prolong the stratum check time to 900s

--- driver-avalon9.c
+++ driver-avalon9.c
@@ -1992,7 +1992,7 @@ static int64_t avalon9_scanhash(struct thr_info *thr)
 
 	/* Step 1: Stop polling and detach the device if there is no stratum in 3 minutes, network is down */
 	cgtime(&current);
-	if (tdiff(&current, &(info->last_stratum)) > 180.0) {
+	if (tdiff(&current, &(info->last_stratum)) > 900.0) {
 		for (i = 1; i < AVA9_DEFAULT_MODULARS; i++) {
 			if (!info->enable[i])
 				continue;
